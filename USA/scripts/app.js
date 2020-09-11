c8c9a45f2cc4387ee630a2d8d49c0667fb2c5e6f
Darcy Atlas <51257058+Darshishka@users.noreply.github.com>
2020-09-11 14:06:38 -0700

    commit for travel

--- USA/scripts/app.js
+++ USA/scripts/app.js
@@ -1,26 +1,22 @@
+const { title } = require("process");
+
 var mapUSA;
 var mapWA;
 var infoWindow;
 var stateName;
 var currState;
 var currStateCoords;
+var currStateData = [];
+var info;
+var contents = [];
+var dataState;
+var nytState;
+var data;
+var cases;
+var y = 0;
 
-function sortData() {
-  for (x = 0; x < nytData.length; x++) {
-    var y = 0; 
-    // if (nytData[x]) {
-//todo
-    // };
-    //all 9,000 data entries
-    console.log(nytData[x]);
-  };
-  var data = nytData[0]["date"];
-  console.log(nytData[0]);
-  console.log(data)
-};
 
 function initMapUSA() {
-  sortData();
   mapUSA = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 39.712162, lng: -95.165789 },
     // disableDefaultUI: true,
@@ -30,25 +26,40 @@ function initMapUSA() {
     mapId: 'f68311c1c85e61',
   });
   for (i = 0; i < state.length; i++) {
-    currState = state[i];
+    currState = state[i]["state"];
     currStateCoords = state[i]["center"];
+    dataState = state[i]["state"];
+    cases;
+    
+    // for (y = 0; y < nytData.length; y++) {
+    //   // console.log(nytData[y]["state"]);
+    //   // console.log(nytData[y]);
+    //   if(nytData[y]["state"] === currState) {
+    //     data[1] = nytData[y]["cases"] + ", ";
+    //     y++;
+    //   } else {
+    //     y++;
+    //   }
+    //   console.log(data);
+    // }
     var marker = new google.maps.Marker({
       position: new google.maps.LatLng(state[i]["center"]),
       map: mapUSA,
-      info: state[i]["id"],
+      title: state[i]["state"],
+      content: cases,
       icon: {
-          path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-          scale: 2
+        path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+        scale: 2
       }
     });
     var infowindow = new google.maps.InfoWindow();
     google.maps.event.addListener(marker, 'mouseover', function () {
-            infowindow.setContent(this.info);                              
-            infowindow.open(map, this);                        
+      infowindow.setContent(this.title);
+      infowindow.open(map, this);                        
     });
     // google.maps.event.addListener(marker, "mouseout", function () {
     //   infoWindow.close(this.info)
     // });  
     marker.setMap(mapUSA);
-  }
-}
+  };
+};
