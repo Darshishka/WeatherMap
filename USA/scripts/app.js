b8c50d8eb876236f6f5e82c5ed54dd65a7a064c9
Darcy Atlas <51257058+Darshishka@users.noreply.github.com>
2020-09-26 19:18:07 -0700

    starting to graph

--- USA/scripts/app.js
+++ USA/scripts/app.js
@@ -1,6 +1,7 @@
 const { title } = require("process");
 const { callbackify } = require("util");
 const { STATUS_CODES } = require("http");
+const e = require("express");
 
 var mapUSA;
 var currState;
@@ -41,34 +42,78 @@ function sortData () {
         stateData.push(value)
       }
     })
-    var marker = new google.maps.Marker({
+//Gets last few 2 of cases to calculate
+    var toCalc = stateData.slice((stateData.length -2), stateData.length);
+    if (toCalc[0]["cases"] < toCalc[1]["cases"]) {
+      console.log(toCalc[0]["state"] + " rise");
+          var marker = new google.maps.Marker({
       position: new google.maps.LatLng(currStateCoords),
       map: mapUSA,
       title: currState,
-      content: stateData,
+      content: `test`,
       icon: {
-        strokeColor: "#FF0000",
-        path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+        strokeColor: "#00FF00",
+        path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
         scale: 2
       }
     });
-    var infowindow = new google.maps.InfoWindow();
-    google.maps.event.addListener(marker, 'mouseover', function () {
-      infowindow.setContent(this.title);
-      infowindow.open(infowindow, this);     
-    });
-    google.maps.event.addListener(marker, `click`, function () {
-      var current = this.title;
-      $.each(nytData, function(index, item) {
-        if (item.state === current) {
-          
+    } else if (toCalc[0]["cases"] > toCalc[1]["cases"]) {
+      console.log(toCalc[0]["state"] + " fall");
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(currStateCoords),
+        map: mapUSA,
+        title: currState,
+        content: stateData,
+        icon: {
+          strokeColor: "#FF0000",
+          path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+          scale: 2
         }
       });
-      infowindow.setContent(this.content);
+      
+    };
+    var infowindow = new google.maps.InfoWindow();
+    google.maps.event.addListener(marker, 'mouseover', function () {
+      infowindow.setContent(this.title + "Graph with last few days of cases" + this.title + ".");
+      infowindow.open(infowindow, this);     
+    });
+    var infoData;
+    google.maps.event.addListener(marker, `click`, function () {
+      var current = this.title;
+      // $.each(nytData, function(index, item) { 
+      //   if (item.state === current) {
+      //     console.log(item);
+      //     infoData += JSON.stringify(item)
+      //   }
+      // });
+      infowindow.setContent(`<div id="myPieChart"/>`);
+      google.charts.load('current', {packages: ['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+      // Define the chart to be drawn.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Element');
+      data.addColumn('number', 'Percentage');
+      data.addRows([
+        ['Nitrogen', 0.78],
+        ['Oxygen', 0.21],
+        ['Other', 0.01]
+      ]);
+
+      // Instantiate and draw the chart.
+      var chart = new google.visualization.PieChart(document.getElementById('myPieChart'));
+      chart.draw(data, null);
+    }
       infowindow.open(marker, this);
+      infoData = [];
     });
     console.log(stateData)
-  }
+
+
+
+    
+  }//for loop
   // var x = 0
   // var i = 0;
   // var y = 0;
@@ -126,4 +171,4 @@ function sortData () {
     //   infowindow.close();
     // });
     marker.setMap(mapUSA);
-};
+};//sortData()
