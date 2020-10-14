e43fa3218ff4f809371594373d6a5c89b9c690b2
Darcy Atlas <51257058+Darshishka@users.noreply.github.com>
2020-10-13 18:12:22 -0700

    data parse working

--- DataParse/app.js
+++ DataParse/app.js
@@ -1,25 +1,27 @@
 function parse() {
     var data = document.getElementById("inputText").value;
     //every space is one point
-    while (data.indexOf(",") != -1) {
-        data = data.replace(",", " ")
-    }
     data = data.split(" ");
+//add to array then shift
     var i;
-    var arrLat = [];
-    var arrLng = [];
+    var output = [];
     for (i = 0; i < data.length; i++) {
-        arrLng += data[i];
-        i++
-        arrLat += data[i];
-        
-    }
-    for (i = 0; i < data.length; i++) {
-        i++
-        i
-    }
-    document.getElementById("output").innerHTML = data;
+        var comma = data[i].indexOf(",");
+        output.push(`{lat: ${data[i].substr(comma+1)}, lng: ${data[i].substr(0,comma)}}`);
+        output.push(`<br>`)
+    };
+    output = output.join(``);
+    console.log(output);
+    document.getElementById("output").innerHTML = output;
+    document.execCommand("copy");
+    
     //need to switch sides of comma
-    console.log(arrLat);
-    console.log(arrLng)
 };
+
+//each space " " is one data point
+    //data is in a string  
+        //identify start of point index
+        //identify space index
+        //substring the inxedes
+        //last index is next starting index
+    //each data point has a Lat and Lng which are in reverse order
